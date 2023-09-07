f99b25adc2fa921b0bfdd99a1b473226b78234af
ikram17 <ikramnassih1017@gmail.com>
2023-09-07 13:00:06 -0700

    0-add,py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
-#!/user/bin/python3
-if__name__=="__main__":
-   from add_0 import add
-   a=1
-   b=2
-   print ("{} + {}").format(a, b, add (a, b))
+#!/usr/bin/python3
+from add_0 import add
+
+a = 1
+b = 2
+
+print("{1} + {2} = {3}".format(a, b, add(a, b)))
