348132a94955a3bc29f160cb826b56be16aa7f32
ikram17 <ikramnassih1017@gmail.com>
2023-09-26 02:45:18 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define a Square class.."""
+
+
+class Square:
+    """Rep Square."""
+
+    def __init__(self, size):
+        """Initial a new "Square".
+
+        Args:
+            size (int): Size of the new "square".
+        """
+        self.__size = size
