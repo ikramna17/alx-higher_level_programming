9c2683a891712022d6b68642dfcc82fb8994739f
ikramna17 <ikramnassih1017@gmail.com>
2023-10-02 09:45:06 +0000

    update

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+"""
+Defines a Rectangle class.
+"""
 
 
 class Rectangle:
-    """Represent a rectangle."""
-    pass
+	"""Represent a rectangle."""
+	pass
