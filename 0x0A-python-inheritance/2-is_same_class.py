60a46805b2df95f056936060d05be9096b0b0246
ikram17 <ikramnassih1017@gmail.com>
2023-10-09 06:38:01 -0700

    lwez

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''Module for is_same class method.'''
 
+
 def is_same_class(obj, a_class):
     '''D.if an obj is exactly an instance of a class.'''
     return type(obj) == a_class
