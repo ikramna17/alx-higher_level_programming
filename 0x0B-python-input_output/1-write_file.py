8c45c42db46b2bf39b166cea4bd7d8a30b2a064d
ikramna17 <ikramnassih1017@gmail.com>
2023-10-10 23:59:54 +0000

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 # 1-number_of_lines.py
 # Brennan D Baraban <375@holbertonschool.com>
-"""Def a text f line-counting fun."""
+"""Defines a text file line-counting function."""
 
 
 def number_of_lines(filename=""):
-    """R the num of lines in a text f."""
+    """Return the number of lines in a text file."""
     lines = 0
     with open(filename) as f:
         for line in f:
